d4c2ad81d8542ee82895963b6795452fd4bd5504
Matt Jared <matt.jared@vercel.com>
2024-07-06 21:13:54 -0500

    Jimmy O'Brien

--- server.js
+++ server.js
@@ -7,44 +7,38 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const USER_AGENT = process.env.USER_AGENT
 
+// const artists = [
+//     { id: 1, name: 'Radiohead', discogsId: 3840 },
+//     { id: 2, name: 'The Beatles', discogsId: 82730 },
+//     { id: 3, name: 'Hall and Oates', discogsId: 95886 },
+// ];
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/genres', async (req, res) => {
+app.get('/albums', async (req, res) => {
+  const artistId = req.query.artist;
   try {
-    const response = await axios.get('https://api.discogs.com/artists/346702', {
-      headers: { 
-        // 'Authorization': `Discogs token=${DISCOGS_API_KEY}`,
-        'User-Agent': USER_AGENT 
-      }
-    });
-    const genres = response.data;
-    console.log(genres, "hey");
-    // const options = genres.map(genre => `<option value="${genre.id}">${genre.name}</option>`).join('');
-    // res.send(`<option value="">--Choose a Genre--</option>${options}`);
+      const response = await axios.get(`https://api.discogs.com/artists/${artistId}/releases`, {
+          headers: { 
+              'User-Agent': USER_AGENT 
+          }
+      });
+      const albums = response.data.releases.filter(release => release.type === 'master').map(release => ({
+          id: release.id,
+          title: release.title
+      }));
+      const options = albums.map(album => `<div class="album">${album.title}</div>`).join('');
+      res.send(`<h2>List of albums:</h2>${options}`);
   } catch (error) {
-    res.status(500).send('Error fetching genres');
+      console.error('Error fetching albums:', error);
+      res.status(500).send('Error fetching albums');
   }
 });
 
-app.get('/subgenres', async (req, res) => {
-  const genreId = req.query.genre;
-  try {
-    const response = await axios.get(`https://api.discogs.com/genres/${genreId}/subgenres`, {
-        headers: { 
-          'User-Agent': USER_AGENT 
-        }
-    });
-    const subgenres = response.data.subgenres;
-    const options = subgenres.map(subgenre => `<option value="${subgenre.id}">${subgenre.name}</option>`).join('');
-    res.send(`<option value="">--Choose a Subgenre/Artist--</option>${options}`);
-  } catch (error) {
-    res.status(500).send('Error fetching subgenres/artists');
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
